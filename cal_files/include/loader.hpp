cba28223ad635c5eb445c05c6bd398a4e9eaa015
Wyatt Barber <wyattjbarber@gmail.com>
2026-02-11 03:47:33 +0000

    Add calibration file loaders

---
+++ cal_files/include/loader.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <opencv2/core/mat.hpp>
+#include <fstream>
+
+
+
+/** Load camera calibration parameters from a file.
+
+Expects the file to be a json file containing the following objects:
+
+*    distortion: A list of floats containing 5 elements
+
+*    intrinsics: A list of floats containing 9 elements of a matrix. 
+        Expects the list to contain all elements of the first row, 
+        followed by all elements of the second row, then the third.
+
+@param file File stream to read from
+@param dist 1x5 distortion coefficient matrix
+@param intr 3x3 intrinsic coefficient matrix
+
+@return Result code, 0 if OK.
+*/
+int load_camera_matrices(std::ifstream& file, cv::Mat& dist, cv::Mat& intr);
+
+
+/** Load stereo camera parameters from a file.
+
+Expects the file to be a json file containing the following objects:
+
+*    translation: A list of floats containing 3 elements
+
+*    rotation: A list of floats containing 9 elements of a matrix. 
+        Expects the list to contain all elements of the first row, 
+        followed by all elements of the second row, then the third.
+
+@param file File stream to read from
+@param R 3x3 rotation matrix
+@param T 3x1 translation matrix
+
+@return Result code, 0 if OK.
+*/
+int load_stereo_matrices(std::ifstream& file, cv::Mat& R, cv::Mat& T);
---
+++ cal_files/src/loader.cpp
@@ -0,0 +1,93 @@
+#include "loader.hpp"
+#include "json/json.h"
+#include <iostream>
+
+
+int load_camera_matrices(std::ifstream& file, cv::Mat& dist, cv::Mat& intr)
+{
+    Json::Value root;
+    Json::CharReaderBuilder builder;
+    JSONCPP_STRING errs;
+    if (!parseFromStream(builder, file, &root, &errs)) {
+        return -1;
+    }
+    if(!root.isMember("distortion") || !root.isMember("intrinsic"))
+    {
+        return -1;
+    }
+
+    auto dist_json = root["distortion"];
+    if(!dist_json.isArray() || !(dist_json.size() == 5))
+    {
+        return -1;
+    }    
+    for(int i = 0; i < 5; i++)
+    {   
+        if(!dist_json[i].isDouble())
+        {
+            return -1;
+        }
+        dist.at<double>(0,i) = dist_json[i].asDouble();
+    }
+
+    auto intr_json = root["intrinsics"];
+    if(!intr_json.isArray() || intr_json.size() != 9)
+    {
+        return -1;
+    }    
+    for(int i = 0; i < 9; i++)
+    {   
+        if(!intr_json[i].isDouble())
+        {
+            return -1;
+        }
+        intr.at<double>(i/3, i%3) = intr_json[i].asDouble();
+    }
+
+    return 0;
+}
+
+
+int load_stereo_matrices(std::ifstream& file, cv::Mat& R, cv::Mat& T)
+{
+    Json::Value root;
+    Json::CharReaderBuilder builder;
+    JSONCPP_STRING errs;
+    if (!parseFromStream(builder, file, &root, &errs)) {
+        return -1;
+    }
+    if(!root.isMember("translation") || !root.isMember("rotation"))
+    {
+        return -1;
+    }
+
+    auto tran_json = root["translation"];
+    if(!tran_json.isArray() || !(tran_json.size() == 3))
+    {
+        return -1;
+    }    
+    for(int i = 0; i < 3; i++)
+    {   
+        if(!tran_json[i].isDouble())
+        {
+            return -1;
+        }
+        T.at<double>(i,0) = tran_json[i].asDouble();
+    }
+
+    auto rot_json = root["rotation"];
+    if(!rot_json.isArray() || rot_json.size() != 9)
+    {
+        return -1;
+    }    
+    for(int i = 0; i < 9; i++)
+    {   
+        if(!rot_json[i].isDouble())
+        {
+            return -1;
+        }
+        R.at<double>(i/3, i%3) = rot_json[i].asDouble();
+    }
+
+    return 0;
+}
